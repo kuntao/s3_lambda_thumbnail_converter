6f8ef6b79830edbab75e1c19a70bd0654ffeb851
kuntao <kuntaro1480@gmail.com>
2016-12-06 20:02:17 +0900

    [mod] メインのコードを修正
    Lambdaで環境変数が使えるようになっていたのでDescriptionから取得するのをやめた
    異なるディレクトリへの出力をやめた

--- lambda_function.py
+++ lambda_function.py
@@ -8,36 +8,25 @@ import re
 
 s3 = boto3.client('s3')
 
-def get_styles(context):
-  client = boto3.client('lambda')
-  response = client.get_function_configuration(FunctionName=context.function_name)
-  desc = json.loads(response['Description'])
-  desc['original'] = None
-  return desc
-
-def random_string(length, seq='0123456789abcdefghijklmnopqrstuvwxyz'):
-  sr = random.SystemRandom()
-  return ''.join([sr.choice(seq) for i in xrange(length)])
-
-def dest_file_path(style):
-  return '%s/%s.jpg' % ('output', style)
+def dest_key(style):
+  key = urllib.unquote(params['Records'][0]['s3']['object']['key']).replace('original', style)
+  print key
+  return key 
 
 def lambda_handler(params, context):
   bucket = params['Records'][0]['s3']['bucket']['name']
-  key = urllib.unquote(params['Records'][0]['s3']['object']['key'])
-  local_path = u'/tmp/' + random_string(16) + os.path.splitext(key)[1]
+  src_key = urllib.unquote(params['Records'][0]['s3']['object']['key'])
+  local_path = u'/tmp/tmp' + os.path.splitext(src_key)[1]
 
-  s3.download_file(Bucket=bucket, Key=key, Filename=local_path)
+  s3.download_file(Bucket=bucket, Key=src_key, Filename=local_path)
   srcimg = Image.open(local_path, 'r')
 
-  styles = get_styles(context)
+  styles = json.loads(os.environ['STYLES'])
   for style in styles:
-    tmp_img = srcimg.copy()
-    if styles[style] is not None:
-      new_size = (styles[style], float(styles[style])/srcimg.size[0]*srcimg.size[1])
-      tmp_img.thumbnail(new_size, Image.ANTIALIAS)
-    tmp_img.save(local_path, 'JPEG')
-    s3.upload_file(Filename=local_path, Bucket=bucket, Key=dest_file_path(style))
+    destimg = srcimg.copy()
+    new_size = (styles[style], float(styles[style])/srcimg.size[0]*srcimg.size[1])
+    destimg.thumbnail(new_size, Image.ANTIALIAS)
+    destimg.save(local_path, 'JPEG')
+    s3.upload_file(Filename=local_path, Bucket=bucket, Key=dest_key(style))
 
-  s3.delete_object(Bucket=bucket, Key=key)
   return
