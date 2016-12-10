32ab3112f7bf0aaa0ceea4e90aadeb3f0ec323a7
kuntao <kuntaro1480@gmail.com>
2016-12-10 17:09:48 +0900

    [mod] コードを修正
    AWS公式のチュートリアルにならってparams['Records']の参照を変更
    などなど

--- lambda_function.py
+++ lambda_function.py
@@ -6,7 +6,7 @@ import re
 
 s3 = boto3.client('s3')
 
-def get_styles:
+def get_sizes():
   p = re.compile('(\w+)=(\d+)')
   dict = {}
   for s in os.environ['STYLES'].split(';'):
@@ -15,25 +15,30 @@ def get_styles:
       dict[m.group(1)] = int(m.group(2))
   return dict
 
-def dest_key(style):
-  key = urllib.unquote(params['Records'][0]['s3']['object']['key']).replace('original', style)
-  print key
-  return key 
+def extract_hash(key):
+  p = re.compile('\/([0-9a-z]{40})\/')
+  return p.search(key).group(1)
 
 def lambda_handler(params, context):
-  bucket = params['Records'][0]['s3']['bucket']['name']
-  src_key = urllib.unquote(params['Records'][0]['s3']['object']['key'])
-  local_path = u'/tmp/tmp' + os.path.splitext(src_key)[1]
 
-  s3.download_file(Bucket=bucket, Key=src_key, Filename=local_path)
-  srcimg = Image.open(local_path, 'r')
+  for record in params['Records']:
+    bucket = record['s3']['bucket']['name']
+    src_key = record['s3']['object']['key']
+    hash_key = extract_hash(src_key)
+    local_path = u'/tmp/tmp' + os.path.splitext(src_key)[1]
 
-  styles = get_styles()
-  for style in styles:
-    destimg = srcimg.copy()
-    new_size = (styles[style], float(styles[style])/srcimg.size[0]*srcimg.size[1])
-    destimg.thumbnail(new_size, Image.ANTIALIAS)
-    destimg.save(local_path, 'JPEG')
-    s3.upload_file(Filename=local_path, Bucket=bucket, Key=dest_key(style))
+    s3.download_file(Bucket=bucket, Key=src_key, Filename=local_path)
+    srcimg = Image.open(local_path, 'r')
+
+    sizes = get_sizes()
+    for style in sizes:
+      new_size = (sizes[style], float(sizes[style])/srcimg.size[0]*srcimg.size[1])
+
+      destimg = srcimg.copy()
+      destimg.thumbnail(new_size, Image.ANTIALIAS)
+      destimg.save(local_path, 'JPEG')
+
+      dest_key = '/thumbnails/%s/%s.jpg' % (hash_key, style)
+      s3.upload_file(Filename=local_path, Bucket=bucket, Key=dest_key)
 
   return
